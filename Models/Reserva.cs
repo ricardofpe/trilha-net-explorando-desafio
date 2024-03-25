febdcd191022026166d3953a43d6c704c0c978d7
Ricardo F Pereira <ricardofonseca_1@hotmail.com>
2024-03-25 15:57:18 -0300

    Desafio .Net Sistema de Hospedagem de hotel concluído

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -17,14 +17,17 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (Suite.Capacidade >= hospedes.Count )
             {
                 Hospedes = hospedes;
+                
             }
             else
             {
+               
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
                 // *IMPLEMENTE AQUI*
+                 throw new Exception("Capacidade máxima de hóspedes excedida.");
             }
         }
 
@@ -34,10 +37,12 @@ namespace DesafioProjetoHospedagem.Models
         }
 
         public int ObterQuantidadeHospedes()
+
         {
+            return Hospedes.Count();
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
             // *IMPLEMENTE AQUI*
-            return 0;
+            
         }
 
         public decimal CalcularValorDiaria()
@@ -45,13 +50,14 @@ namespace DesafioProjetoHospedagem.Models
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
             // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+            
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                return valor - valor / 10;
             }
 
             return valor;
